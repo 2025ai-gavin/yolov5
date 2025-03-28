c6a09f71a5bbd5deedaf92b6cf3b58abce1df372
gavin.feng <gavin>
2025-03-28 08:56:09 +0800

    添加注释，查看层数及特征图大小

--- models/yolo.py
+++ models/yolo.py
@@ -161,12 +161,18 @@ class BaseModel(nn.Module):
     def _forward_once(self, x, profile=False, visualize=False):
         """Performs a forward pass on the YOLOv5 model, enabling profiling and feature visualization options."""
         y, dt = [], []  # outputs
+        i=1
         for m in self.model:
             if m.f != -1:  # if not from previous layer
                 x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
             if profile:
                 self._profile_one_layer(m, x, dt)
             x = m(x)  # run
+            try:
+                print('层数：',i,'特征图大小：',x.shape)
+            except:
+                pass
+            i+=1
             y.append(x if m.i in self.save else None)  # save output
             if visualize:
                 feature_visualization(x, m.type, m.i, save_dir=visualize)
